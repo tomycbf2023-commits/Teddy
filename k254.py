51937fa7310484206c085200b84dccb6bd7a939f
tomycbf2023-commits <tomycbf2023@gmail.com>
2026-03-13 19:30:39 +0800

    Refactor clap counting in k254.py
    Refactor clap counting logic and simplify conditions.

--- k254.py
+++ k254.py
@@ -1,18 +1,13 @@
 #k254. 拍七 (Seven)
-def haveb(i,b): # 若 i 內出現 b 則回傳 T 否則回傳 F
-    s = str(i)
-    for a in s:
-        if int(a) == b:
-            return True
-    return False
-
 s,e,b,k = map(int,input().split())
-p = 0 # 拍手次數
+clap = 0
 for i in range(s,e+1):
-    if i%b==0 or haveb(i,b):
-        p += 1
-        if p == k :
-            print(i)
-            break
-if p != k:
-    print(-1)
+    if i % b == 0 or str(b) in set(str(i)):
+        clap += 1
+    if clap == k:
+        res = i
+        break
+else:
+    res = -1
+
+print(res)
